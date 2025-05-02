bb3e0baa36bd4e1f20c708d430837fd3e0af5cce
Ankur Singh <21uec154@lnmiit.ac.in>
2025-05-02 15:07:22 +0530

    Added project files

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+  ,
+
+)
